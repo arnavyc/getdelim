857f2e1c3c7c7799bbb02d26655e692ce85fc68e
arnavyc <ayc-vcs.m9xhr@aleeas.com>
2023-08-15 22:39:14 +0530

    refactor: merge cmake/BuildType.cmake into cmake/DefaultConfig.cmake

--- cmake/DefaultConfig.cmake
+++ cmake/DefaultConfig.cmake
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2021 arnavyc
-#
 # SPDX-License-Identifier: 0BSD
 
 # Sets default, "sane" compiler and CMake options in the project
@@ -20,3 +18,17 @@ add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/source-charset:utf-8>")
 # MSVC: Enable __cplusplus to report updated value for recent C++ standards.
 add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/Zc:__cplusplus>")
 
+# Set a default build type if none was specified
+set(default_build_type "Release")
+if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+  set(default_build_type "Debug")
+endif()
+
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+      STRING "Choose the type of build." FORCE)
+  # Set the possible values of build type for cmake-gui
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
+    "MinSizeRel" "RelWithDebInfo")
+endif()
